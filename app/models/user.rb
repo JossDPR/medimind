b26d883b2d0bc2d75e7585d4bd199e703813e117
Josselin Dupire <josselin.dupire@gmail.com>
2024-08-20 15:28:04 +0200

    add role_route_constraint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_create :default_tutor
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def default_tutor
+    @role ||= "tutor"
+  end
+
+  def tutor?
+    @role == "tutor"
+  end
+
+  def patient?
+    @role == "patient"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get '/', to: 'tutor#home', constraints: RoleRouteConstraint.new { |user| user.tutor? }
+  get '/', to: 'patient#home', constraints: RoleRouteConstraint.new { |user| user.patient? }
 
   resources :patients, only: [:index, :new, :create, :edit, :update, :destroy]
 end
