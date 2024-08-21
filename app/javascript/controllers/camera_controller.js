96ed7e97b588b3e0b6861d6c26c250aa385351a2
Arthur-Henri Michalland <ah.michalland@gmail.com>
2024-08-21 16:35:29 +0200

    photo working

--- app/javascript/controllers/camera_controller.js
+++ app/javascript/controllers/camera_controller.js
@@ -6,19 +6,26 @@ export default class extends Controller {
 
   connect() {
     console.log("Camera controller is in tha place!");
-    const video = this.cameraScreenTarget;
+    // const video = this.cameraScreenTarget;
 
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(function(stream) {
-      video.srcObject = stream;
-      video.play();
+    .then(stream => {
+      // video.srcObject = stream;
+      // video.play();
+      this.stream=stream
+      this.cameraScreenTarget.srcObject = stream;
+      this.cameraScreenTarget.play();
+      const tracks = stream.getTracks();
+      // tracks[0].stop;
     })
     .catch(function(err) {
       console.log("An error occurred: " + err);
     });
 
-    video.setAttribute("height",400);
-    video.setAttribute("width",400);
+    // video.setAttribute("height",400);
+    // video.setAttribute("width",400);
+    this.cameraScreenTarget.setAttribute("height",400);
+    this.cameraScreenTarget.setAttribute("width",400);
 
     };
 
@@ -26,21 +33,25 @@ export default class extends Controller {
     const canvas = document.createElement('canvas');
     let width = 400;    // We will scale the photo width to this
     let height = 400;
-    const video = this.cameraScreenTarget;
+    // const video = this.cameraScreenTarget;
     // console.log(video)
     var context = canvas.getContext('2d');
     canvas.width = 400;
     canvas.height = 400;
-    context.drawImage(video, 0, 0, width, height);
+    context.drawImage(this.cameraScreenTarget, 0, 0, width, height);
 
     let data = await new Promise((resolve) => {
       canvas.toBlob(resolve, 'image/png');
     });
 
-    this.cameraButtonTarget.innerText = "✅";
-    video.pause();
-    video.currentTime = 0;
-
+    this.cameraButtonTarget.classList.add("d-none");
+    // video.pause();
+    // video.currentTime = 0;
+    // this.cameraScreenTarget.pause();
+    // this.cameraScreenTarget.currentTime=0;
+    this.stream.getTracks().forEach(track => {
+      track.stop();
+    });
 
   };
 }
