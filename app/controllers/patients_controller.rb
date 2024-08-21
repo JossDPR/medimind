b21a6cec39092d39f30fcb92b7acfb331dd463d3
Josselin Dupire <josselin.dupire@gmail.com>
2024-08-21 15:40:28 +0200

    add links to patients list

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -17,7 +17,6 @@ class PatientsController < ApplicationController
     # authorize @patient
     @patient = User.new(patient_params)
     @user.patients.push(@patient)
-    raise
     if @patient.save
       redirect_to patient_planifications_path(@patient)
     else
