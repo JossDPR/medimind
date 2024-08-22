d24667290b4936ab72e492c135ef947cf5b711f2
Arthur-Henri Michalland <ah.michalland@gmail.com>
2024-08-22 15:34:10 +0200

    Update planification.rb

--- app/models/planification.rb
+++ app/models/planification.rb
@@ -2,7 +2,7 @@ class Planification < ApplicationRecord
   belongs_to :medication
   belongs_to :dosage
   belongs_to :patient, :class_name => 'User'
-  belongs_to :plan_taking
+  has_many :plan_takings
   has_many :taking_periods, through: :plan_taking
   has_many :takes
   has_one_attached :photo
