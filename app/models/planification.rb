f780addd6fa8c21045918a5fe954748e8359045c
Andjiane Raveloson <andrianina_raveloson@hotmail.fr>
2024-08-22 15:27:51 +0200

    relation des tables + init confirmation page
    Signed-off-by: Andjiane Raveloson <andrianina_raveloson@hotmail.fr>

--- app/models/planification.rb
+++ app/models/planification.rb
@@ -2,5 +2,8 @@ class Planification < ApplicationRecord
   belongs_to :medication
   belongs_to :dosage
   belongs_to :patient, :class_name => 'User'
+  belongs_to :plan_taking
+  has_many :taking_periods, through: :plan_taking
+  has_many :takes
   has_one_attached :photo
 end
--- app/models/take.rb
+++ app/models/take.rb
@@ -1,2 +1,3 @@
 class Take < ApplicationRecord
+  belongs_to :planification
 end
