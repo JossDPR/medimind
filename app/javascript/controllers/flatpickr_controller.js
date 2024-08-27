8be43aed702ce9900befa73bea46bbadd093a639
Josselin Dupire <josselin.dupire@gmail.com>
2024-08-27 09:30:18 +0200

    flatpickr ok

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,9 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
-import flatpickr from "flatpickr"
+import flatpickr from 'flatpickr';
 
-// Connects to data-controller="flatpickr"
 export default class extends Controller {
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.element, {
+      minDate: "today",
+      dateFormat: "d-m-Y",
+      "locale": {
+      "firstDayOfWeek": 1
+      },
+    })
   }
 }
